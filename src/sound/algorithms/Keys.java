5f25b734521c187148852379802ce71e547556de
Beatriz Fusaro <beatrizfusaro@gmail.com>
2017-04-14 17:38:55 -0400

    Started processing algorithm and Kays class

---
+++ src/sound/algorithms/Keys.java
@@ -0,0 +1,16 @@
+package sound.algorithms;
+
+/**
+ * Created by Beatriz on 14/04/2017.
+ */
+public class Keys {
+    private double preco;
+
+    public Keys(double preco){
+        this.preco = preco;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+}
