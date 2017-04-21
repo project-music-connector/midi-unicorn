1cb0509a43606a1d0d4552574a15c7674552dd88
Beatriz Fusaro <beatrizfusaro@gmail.com>
2017-04-21 17:15:50 -0400

    Implemented Keys.java and UnicornParser.java

--- src/sound/algorithms/Keys.java
+++ src/sound/algorithms/Keys.java
@@ -3,14 +3,33 @@ package sound.algorithms;
 /**
  * Created by Beatriz on 14/04/2017.
  */
-public class Keys {
-    private double preco;
+public enum Keys {
+    C_MJ(0, 0), C_SHARP_MJ(1, 0), D_MJ(3, 0), D_SHARP_MJ(4, 0), E_MJ(5, 0),
+    F_MJ(6, 0), F_SHARP_MJ(7, 0), G_MJ(8, 0), G_SHARP_MJ(9, 0), A_MJ(10, 0),
+    A_SHARP_MJ(11, 0), B_MJ(12, 0), B_FLAT_MJ(11, 0), A_FLAT_MJ(9, 0),
+    G_FLAT_MJ(7, 0), E_FLAT_MJ(4, 0), D_FLAT_MJ(1, 0),
+    C_MN(0, 1), C_SHARP_MN(1, 1), D_MN(3, 1), D_SHARP_MN(4, 1), E_MN(5, 1),
+    F_MN(6, 1), F_SHARP_MN(7, 1), G_MN(8, 1), G_SHARP_MN(9, 1), A_MN(10, 1),
+    A_SHARP_MN(11, 1), B_MN(12, 1), B_FLAT_MN(11, 1), A_FLAT_MN(9, 1),
+    G_FLAT_MN(7, 1), E_FLAT_MN(4, 1), D_FLAT_MN(1, 1);
 
-    public Keys(double preco){
-        this.preco = preco;
+    private int value;
+    private int quality;
+
+    Keys(int value, int quality) {
+        this.value = value;
+        this.quality = quality;
     }
 
-    public double getPreco() {
-        return this.preco;
+    public int getValue() {
+        return value;
+    }
+
+    public Chord[] getRelationTable() {
+        if (quality == 0){
+            return [Chord.MAJOR, ....];
+        } else {
+            return [Chord.MINOR, ....];
+        }
     }
 }
--- src/sound/algorithms/UnicornParser.java
+++ src/sound/algorithms/UnicornParser.java
@@ -3,5 +3,16 @@ package sound.algorithms;
 /**
  * Created by Asimm on 4/7/2017.
  */
+
 public class UnicornParser {
+
+    public void parse(int[][] input, Keys key) {
+        for (int i = 0; i < input[0].length; i++) {
+            int j = input.length - 1;
+
+        }
+    }
+
+
 }
+
