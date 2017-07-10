ee4c6115dd86a64b5d74741a1531ee293db7302b
Beatriz Fusaro <beatrizfusaro@gmail.com>
2017-07-10 22:05:11 +0200

    Update Key.java

--- src/sound/gen/Key.java
+++ src/sound/gen/Key.java
@@ -4,13 +4,13 @@ package sound.gen;
  * Created by Beatriz Fusaro on 7/8/2017.
  */
 public enum Key {
-    C(1),
-    D(2),
-    E(3),
-    F(4),
-    G(5),
-    A(6),
-    B(7);
+    C(0),
+    D(1),
+    E(2),
+    F(3),
+    G(4),
+    A(5),
+    B(6);
     private int key;
     Key(int key) {
         this.key = key;
